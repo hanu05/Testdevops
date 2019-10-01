84b82810c668b73227d4247c229cb6729562bd86
v-rachai <v-rachai@INFHYSZAL04>
2019-10-01 11:32:36 +0530

    Added New Project..

---
+++ Testdevops/Testdevops/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testdevops.Startup))]
+namespace Testdevops
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
